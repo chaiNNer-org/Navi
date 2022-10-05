15cb565afa3fdac032a35de6e5c4cbe990a17a27
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-10-05 14:50:44 +0200

    Use constants for better performance

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+import { canonicalize } from './canonical';
+import { IntIntervalType, IntervalType, NumericLiteralType, StructType, UnionType } from './types';
+
+export const NAN = new NumericLiteralType(NaN);
+export const INF = new NumericLiteralType(Infinity);
+export const NEG_INF = new NumericLiteralType(-Infinity);
+export const ZERO = new NumericLiteralType(0);
+export const ONE = new NumericLiteralType(1);
+
+export const REAL = new IntervalType(-Infinity, Infinity);
+export const INT = new IntIntervalType(-Infinity, Infinity);
+
+export const BOOL_TRUE = new StructType('true');
+export const BOOL_FALSE = new StructType('false');
+export const BOOL = new UnionType(canonicalize([BOOL_FALSE, BOOL_TRUE]));
