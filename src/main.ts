bfefc7e44a8b4b6577d8f2a4ea6d468cd80e266a
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-10-05 14:08:17 +0200

    Added main.ts

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+export * from './evaluate';
+export * from './expression';
+export * from './scope';
+export * from './source';
+export * from './types';
+
+export { getReferences } from './expression-util';
+export { globalScope } from './global-scope';
+export {
+    intInterval,
+    interval,
+    isNumericLiteral,
+    isSameType,
+    isStringLiteral,
+    literal,
+} from './types-util';
+export { intersect, isDisjointWith } from './intersection';
+export { isSubsetOf, isSupersetOf } from './relation';
+export { parseDefinitions, parseExpression } from './parse';
+export { union } from './union';
+export { without } from './without';
