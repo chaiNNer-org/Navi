eb5b82b5d203f44ff755633caafc8532f1f01f07
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-12-06 21:02:42 +0100

    Added assertions for bool structs

--- src/builtin/bool.ts
+++ src/builtin/bool.ts
@@ -1,10 +1,24 @@
 import { BOOL_FALSE, BOOL_TRUE } from '../constants';
+import { StructType } from '../types';
 import { wrapReducerVarArgs } from './wrap';
 
-export const and = wrapReducerVarArgs(BOOL_TRUE, (a, b) => {
+const assertBool: (type: StructType) => asserts type is StructType & {
+    readonly name: 'true' | 'false';
+    readonly fields: readonly [];
+} = (type) => {
+    if (type.fields.length !== 0 || (type.name !== 'true' && type.name !== 'false')) {
+        throw new Error(`The type ${type.toString()} is not a value bool type`);
+    }
+};
+
+export const and = wrapReducerVarArgs(BOOL_TRUE, (a: StructType, b: StructType) => {
+    assertBool(a);
+    assertBool(b);
     return a.name === 'true' && b.name === 'true' ? BOOL_TRUE : BOOL_FALSE;
 });
 
 export const or = wrapReducerVarArgs(BOOL_TRUE, (a, b) => {
+    assertBool(a);
+    assertBool(b);
     return a.name === 'true' || b.name === 'true' ? BOOL_TRUE : BOOL_FALSE;
 });
