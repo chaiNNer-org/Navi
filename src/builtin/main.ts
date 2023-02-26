7d5b0e1f3f819eee745c9254d46c21fb2066dec1
Michael Schmidt <msrd0000@gmail.com>
2023-02-26 19:37:39 +0100

    Fixed export of builtin (#34)

--- src/builtin/main.ts
+++ src/builtin/main.ts
@@ -1,7 +1,14 @@
-export * from './any';
-export * from './bool';
-export * from './number-compare';
-export * from './number';
-export * from './string';
+import { StringPrimitive } from '../types';
+import { concat } from './string';
+import { ReducerVarArgsFn } from './wrap';
+
 export * from './wrap';
 export { handleNumberLiterals } from './util';
+
+const Intrinsic = {
+    // the type cast is necessary to work around a bug in parcel
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    concat: concat as ReducerVarArgsFn<StringPrimitive>,
+} as const;
+
+export { Intrinsic };
