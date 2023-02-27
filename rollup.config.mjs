a63d3f3a4d8d4cd3e236cf094e99165097b479ed
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-02-27 16:20:58 +0100

    Inlined antlr4

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import run from '@rollup/plugin-run';
 import typescript from '@rollup/plugin-typescript';
 import fs from 'fs';
 
@@ -8,7 +9,6 @@ const globalNavi = fs.readFileSync('src/global.navi', 'utf-8');
 /** @type {import("rollup").RollupOptions} */
 const config = {
     input: 'src/main.ts',
-    external: [/^antlr4$/],
     output: [
         {
             file: 'dist/main.js',
@@ -16,7 +16,7 @@ const config = {
             sourcemap: true,
         },
         {
-            file: 'dist/module.js',
+            file: 'dist/module.mjs',
             format: 'module',
             sourcemap: true,
         },
@@ -24,13 +24,15 @@ const config = {
     plugins: [
         replace({
             values: {
-                "import fs from 'fs'": '',
                 "fs.readFileSync(__dirname + '/global.navi', 'utf-8')": JSON.stringify(globalNavi),
             },
             delimiters: ['', ''],
         }),
         nodeResolve({ extensions: ['.mjs', '.js', '.json', '.ts'] }),
         typescript(),
+        run({
+            execArgv: ['-r', 'source-map-support/register'],
+        }),
     ],
 };
 
